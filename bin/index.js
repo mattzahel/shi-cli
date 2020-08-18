bd695d2985746652e1cb5be108206c9fc3b0d2ce
Mateusz Zahel <mateusz.zahel@gmail.com>
2020-08-18 22:03:17 +0200

    update programs list

--- bin/index.js
+++ bin/index.js
@@ -15,8 +15,20 @@ program
 
 program
   .command('system')
-  .alias('a')
-  .description('Displays information system NoFluffJobs project')
+  .alias('sys')
+  .description('Displays information about operating system')
+  .action(() => system())
+
+program
+  .command('filesystem')
+  .alias('fs')
+  .description('Displays information about filesystem')
+  .action(() => system())
+
+program
+  .command('user')
+  .alias('usr')
+  .description('Displays information about current user')
   .action(() => system())
 
 program.parse(process.argv)
